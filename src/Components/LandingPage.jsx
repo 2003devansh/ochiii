4b17599565f53574ef1cb0db0728d477ffbf3975
2003devansh <faujdardevansh@gmail.com>
2024-07-29 15:51:47 +0530

    commiting chnages

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -7,18 +7,27 @@ const LandingPage = () => {
    <div className='w-full h-screen bg-zinc-900 pt-1'>
     <div className='textstructure mt-40 px-20'>
     {["we create" , "Eye Opening " , "Presentation "].map((item, index)=>{
-      return <div className='masker text-9xl font-semibold'>
-        <h1 className='uppercase text-[7.5vw] leading-[7vw] tracking-lighter font-Helvetica'>{item}</h1>
+      return (
+        <div className='masker'>
+        <div className='w-fit flex '>
+          {index === 1  && (<div className='w-[9vw] h-[vw] relative -top-[1vw] bg-red-500'></div>)}
+          <h1 className=" flex items-center uppercase text-[9vw] h-full leading-[7vw] font-serif">{item}</h1>
+        </div>
       </div>
+      ); 
     })}
     </div>
     <div className="border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center py-5 px-20">
       {["see it through","i am the choosen one"].map((item,index)=>{
         return <p className='text-md font-thin tracking-tight leading-none'>{item}</p>
       })}
-      <div className='start'>
+      <div className='start flex items-center gap-5'>
         <button className='px-4 border-[1] border rounded-full'>Start the thing</button>
-        <div className='w-3 h-3 rounded-full'><FaArrowUp /> </div>
+        <div className='w-10 h-10 flex items-center justify-center border-[2px] border-zinc-500 rounded-full'>
+        <span className='rotate-[90deg]'>
+        <FaArrowUp />
+        </span>
+        </div>
       </div>
     </div>
    </div>
